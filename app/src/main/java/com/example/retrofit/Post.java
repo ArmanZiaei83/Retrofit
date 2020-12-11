04e1566e940643145a867d14a188a958171662cc
armanziaei <armanziaei83@gmaio.com>
2020-12-11 21:49:42 +0330

    Feats For Post.java : Some features added

--- app/src/main/java/com/example/retrofit/Post.java
+++ app/src/main/java/com/example/retrofit/Post.java
@@ -1,4 +1,24 @@
 package com.example.retrofit;
 
 public class Post {
+    private  String title ;
+    private  String text ;
+    private int id ;
+    private int userId ;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
 }
